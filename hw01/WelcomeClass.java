d002be50d5bf20e88a22afb619c7b8d006de84e0
ravidenze <gdm317@lehigh.edu>
2014-09-03 02:01:17 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//Grant Moore
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass
+//define class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[] args) {
+
+//  print the statement with the box around it
+    System.out.println("   -----------------   ");
+    System.out.println("   |    WELCOME    |   ");
+    System.out.println("   -----------------   ");
+    System.out.println("    ^  ^  ^  ^  ^  ^   ");
+    System.out.println("   / \\/ \\/ \\/ \\/ \\/ \\  ");
+    System.out.println("  |-G--D--M--3--1--7-| ");
+    System.out.println("   \\ /\\ /\\ /\\ /\\ /\\ /  ");
+    System.out.println("    v  v  v  v  v  v   ");
+    System.out.println("I love the outdoors, helping others, and good food. ");
+    
+    }
+}
