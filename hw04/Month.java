45fda83429e577d7cc5a94cad735810f1d73a3cf
ravidenze <gdm317@lehigh.edu>
2014-09-24 02:45:43 +0000

    updated Homework 4 with Month

--- hw04/Month.java
+++ hw04/Month.java
@@ -21,49 +21,45 @@ import java.util.Scanner; //Sets up the ability of inputs from the user
             Scanner myScanner1;
             myScanner1 = new Scanner( System.in );
             
-            Scanner myScanner2;
-            myScanner2 = new Scanner( System.in );
-             //double IntCheck = nForTax-((int)nForTax);
-                   double Year;
+            
                 
                    
             
                     //Variables for user input and their respective instructions
                     System.out.print("Enter an integer representing a month: ");
+                         int nForMonth = myScanner1.nextInt();//Actually taking in the number for 
                         
-                        
-                       
-                        if (myScanner.hasNextInt()){
-                            int nForMonth = myScanner1.nextInt();//Actually taking in the number for 
                              
-                                     if ((nForMonth < 1) || (nForMonth>12) { 
+                                   if ((nForMonth < 1) || (nForMonth>12)) { 
                                        System.out.println("You did not enter a valid month.");//Printing out an error on the users part
                                        return;
                                    }
                            
-                                   if (nForMonth = (1,3,5,7,8,10,12) {
-                                       System.out.println("This month has 31 days);//Printing how many days are in these months
+                                   if ((nForMonth == 1) || (nForMonth == 3) || (nForMonth == 5) || (nForMonth == 7) || (nForMonth == 8) || (nForMonth == 10) || (nForMonth == 12)) {//checking for months
+                                       System.out.println("This month has 31 days");//Printing how many days are in these months
                                    }
                                    
-                                   if (nForMonth = (4,6,9,11)) {
-                                       System.out.println("This month has 28 days);//Printing how many days are in these months
+                                   if ((nForMonth == 4) || (nForMonth == 6) || (nForMonth == 9) || (nForMonth == 11)) {//chekcing for months
+                                       System.out.println("This month has 28 days");//Printing how many days are in these months
                                    }
                                   
-                                   if (nForMonth = 2)  {
-                                       int nForYear = myScanner2.nextInt();//Taking a number for the year
+                                   if (nForMonth == 2)  {
+                                       //setting up a second scanner for the year ebcause feb
+                                       Scanner myScanner2;
+                                       myScanner2 = new Scanner( System.in );
                                        
-                                       
+                                            System.out.print("Enter an integer giving the year: ");//Finishing scanner set up by asking user
+                                            int nForYear = myScanner2.nextInt();//Taking a number for the year
+                                            boolean isLeapYear = ((nForYear % 4 == 0) && (nForYear % 100 != 0) || (nForYear % 400 == 0));//computation for leap year
+
+                                        if (isLeapYear)//self explanatory
+                                        {
+                                            System.out.println("This month has 29 days this year");//Printing how many days are in this month during a leap year
+                                        } else
+                                            System.out.println("This month has 28 days this year");//Printing how many days are in this month not a leap year
                                    }
                                    
-                                   
-                           
-                        }
-                        
-                       
-                        else {
-                             System.out.println("You did not enter an integer -- can not display number of days for a non-existent month.");//Printing out an error on the users part
-                             return;
-                        }
+
                         
     }   //End of the main method
 } //End of class
